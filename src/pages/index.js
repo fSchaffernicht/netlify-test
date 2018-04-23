1ae608e79a844856a3129ef622c642e56e9f2bb9
Felix Schaffernicht <felix@schaffernicht.com>
2018-04-23 23:53:04 +0200

    add styled-components and new blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,45 @@
 import React from "react"
+import Link from 'gatsby-link'
+import styled from 'styled-components'
 
-export default () => <div>Hello world update! Jetzt habe ich grade mal was geändert was ich live haben will.</div>
+const Wrapper = styled.div`
+  padding: 2rem;
+`
+
+const StyledLink = styled(Link)`
+  color: pink;
+  font-size: 2rem;
+`
+
+export default (props) => {
+  console.log('props', props)
+  const { data } = props
+  const { allSitePage } = data 
+  return (
+    <div>
+      {
+        allSitePage.edges.map((item, index) => {
+          return (
+            <Wrapper key={index}>
+              <StyledLink to={item.node.id.substring(9)}>
+                {item.node.id}
+              </StyledLink>
+            </Wrapper>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export const pageQuery = graphql`
+  query Test {
+    allSitePage {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+`;
